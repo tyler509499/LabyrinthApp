576471d60293879441debe9c2abf2f4d1d1145c5
Nikita Galkov <n.galkov@growapp.biz>
2021-08-14 16:43:10 +0300

    Initial Commit

---
+++ LabyrinthApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LabyrinthApp
+//
+//  Created by Galkov Nikita on 14.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
